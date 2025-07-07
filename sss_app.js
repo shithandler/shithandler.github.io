3e8541ece53cf44532bdd12c8e1e58df1937fed4
shithandler <kristijancrnjak2@gmail.com>
2025-07-07 14:27:10 +0200

    fix sss_app.js

--- sss_app.js
+++ sss_app.js
@@ -183,7 +183,7 @@ async function conversation() {
     else{modelused = "deepseek-chat"}
     entered = entered.replace(/gosmart/g, "")
 
-    if (!isNaN(parseFloat(entered))){ tmptr = parseFloat(entered); 
+    if (entered.length == 3 && !isNaN(parseFloat(entered))){ tmptr = parseFloat(entered); 
     console.log(modelused)
     console.log(tmptr)
     continue}
